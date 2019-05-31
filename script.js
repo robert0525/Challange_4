7b36991db9ed30ae82fc24c9191fa8ea473134cf
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-31 22:43:37 +0200

    finnal commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     var volume;
     var radius = document.getElementById("radius").value;
     radius = Math.abs(radius);
-    volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    volume = Math.floor((4/3) * Math.PI * Math.pow(radius, 3));
     volum = volume.toFixed(4);
     document.getElementById('volume').value = volume;
     return false;
